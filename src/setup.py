870930b1229c2a9f2ac2ad79a8dc3f3965afda91
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-12-27 22:15:00 +0800

    Adding basic structure.

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,5 @@
 # import argparse
+import os
 import sys
 import types
 from io  import StringIO
@@ -18,18 +19,19 @@ class XMEnv:
 	def setupEnv(self):
 		# abort for non-empty dst
 		# substitute %--XM_*% template variable (%--XM_ENVNAME%, %--XM_ENVPATHS%)
+		# inject environment path (root, root/scripts)
 		# copy ['pip', 'python', 'python_d', 'pythonw', 'pythonw_d']
 		
 		# prepare env & rt
 		try:
 			rt_exe = Path(self.envrt)
-			rt_dir = rt_exe.parent
+			rt_dir = rt_exe.parent.resolve()
 			assert( rt_dir.is_dir() )
 		except:
 			raise # ValueError('Invalid Python runtime.')
 		
 		try:
-			env_dir = Path(self.envdest)
+			env_dir = Path(self.envdest).resolve()
 			assert( not env_dir.is_file() )
 		except:
 			raise # ValueError('Invalid destination.')
@@ -37,11 +39,19 @@ class XMEnv:
 			if env_dir.is_dir() and len([*env_dir.iterdir()]) > 0:
 				raise ValueError('Destination is not empty.')
 		
+		
 		# copy runtime binaries
 		copy_list = {'': ['python', 'python_d', 'pythonw', 'pythonw_d'], 'scripts': ['pip']}
 		
-		# pack templates
 		
+		# pack templates
+		try:
+			script_dir = (Path(__file__).parent / 'scripts').resolve()
+			assert(script_dir.is_dir())
+		except:
+			raise
+		
+		paths = os.environ['path'].split(os.pathsep)
 		
 		return
 
@@ -55,8 +65,12 @@ class ArgvParser:
 		usage = 'setup [--help] dest'
 		try:
 			cls.dest = argv[0]
-		except:
-			raise ValueError('Usage: {}'.format(usage))
+			# Usage help
+		except Exception as e:
+			print('Parser exit with excecption: {}({}).'.format(type(e).__name__, e), file=sys.stderr)
+			print(ValueError('Usage: {}\n'.format(usage)), file=sys.stderr)
+			
+			return None
 		
 		return cls
 
@@ -64,8 +78,9 @@ def runWithOptions(options):
 	parser = ArgvParser()
 	arg = parser.parse(options)
 	
-	env = XMEnv(arg.dest, sys.executable)
-	env.setupEnv()
+	if arg != None:
+		env = XMEnv(arg.dest, sys.executable)
+		env.setupEnv()
 
 if __name__ == "__main__":
 	runWithOptions(sys.argv[1:])
